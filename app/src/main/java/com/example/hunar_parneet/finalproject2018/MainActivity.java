19a2f2786ac73b897b59d264362e611c5a90dfce
wjbernal <43277847+wjbernal@users.noreply.github.com>
2018-11-14 19:02:23 -0500

    milestone 1 complete

--- app/src/main/java/com/example/hunar_parneet/finalproject2018/MainActivity.java
+++ app/src/main/java/com/example/hunar_parneet/finalproject2018/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.hunar_parneet.finalproject2018;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+private Button buttonStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonStart = (Button)findViewById(R.id.Button1);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,ListNewsActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
